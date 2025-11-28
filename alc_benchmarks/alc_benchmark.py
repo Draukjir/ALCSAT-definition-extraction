7ed5b9ac745197e6397f997551c56ad493154e03
mfunk <mfunk@informatik.uni-leipzig.de>
2025-11-28 09:48:52 +0100

    Move SML stuff to alc_benchmark

--- alc_benchmarks/alc_benchmark.py
+++ alc_benchmarks/alc_benchmark.py
@@ -12,9 +12,10 @@ from owlready2 import default_world, get_ontology
 from rdflib import Graph
 
 from spell.benchmark_tools import construct_owl_from_structure
+from spell.fitting import determine_relevant_symbols
 from spell.fitting_alc import FittingALC
 from spell.structures import Signature, Structure, map_ind_name, structure_from_owl
-from spell.instance import ALC_OP, OP
+from spell.instance import ALC_OP, OP, Instance
 from spell.preprocessing import color_refinement
 
 from spell.preprocessing import restrict_to_neighborhood
@@ -590,6 +591,143 @@ def examples_from_bisim(kb_path, output_dir):
             )
             with open(os.path.join(dest_dir, "fitting_concept.txt"), "w") as f:
                 f.write(sol.to_tree())
+
+
+def chunks(lst: list[int], n: int):
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+def kfold(inst: Instance, folds: int = 5, max_k=10, timeout: float = 30):
+    all_p = list(inst.P)
+    all_n = list(inst.N)
+
+    random.shuffle(all_p)
+    random.shuffle(all_n)
+
+    # TODO: aufrunden
+    p_chunks = list(chunks(all_p, len(all_p) // folds))
+    n_chunks = list(chunks(all_n, len(all_n) // folds))
+
+    accuracies: list[float] = []
+    accuracies2: list[float] = []
+    f1scores2: list[float] = []
+    sizes: list[float] = []
+    sizes_evo: list[float] = []
+
+    for i in range(folds):
+        this_p = [p for j in range(folds) for p in p_chunks[j] if j != i]
+        this_n = [n for j in range(folds) for n in n_chunks[j] if j != i]
+
+        f = FittingALC(inst.A, max_k, this_p, this_n, inst.op, 8, 2)
+        (acc, n, concept) = f.solve_incr_approx(max_k, timeout=timeout)
+        accuracies.append(acc)
+        sizes.append(concept.size())
+        sizes_evo.append(concept.evo_size())
+
+        other_p = p_chunks[i]
+        other_n = n_chunks[i]
+
+        tp = 0
+        fp = 0
+        tn = 0
+        fn = 0
+
+        for p in other_p:
+            if concept.mc(inst.A, p):
+                tp += 1
+            else:
+                fn += 1
+        for n in other_n:
+            if concept.mc(inst.A, n):
+                fp += 1
+            else:
+                tn += 1
+
+        acc2 = (tp + tn) / (tp + fn + fp + tn)
+        f1 = (2 * tp) / (2 * tp + fp + fn)
+        f1scores2.append(f1)
+        accuracies2.append(acc2)
+
+    avg_acc = sum(accuracies) / len(accuracies)
+    print(f"Average accuracy on training data: {avg_acc:.4f}")
+
+    avg_size = sum(sizes) / len(sizes)
+    print(f"Average size of concept: {avg_size:.4f}")
+
+    avg_evo_size = sum(sizes_evo) / len(sizes_evo)
+    print(f"Average evolearner size of concept: {avg_evo_size:.4f}")
+
+    avg_acc2 = sum(accuracies2) / len(accuracies2)
+    print(f"Average accuracy on test data: {avg_acc2:.4f}")
+
+    avg_f1 = sum(f1scores2) / len(f1scores2)
+    print(f"Average F1score on test data: {avg_f1:.4f}")
+
+    return (avg_acc2, avg_f1, avg_size, avg_evo_size)
+
+
+def sml_benchmark_cross_validate(resultpath: str):
+    with open(resultpath, mode="w") as outfile:
+        _ = outfile.write("bench, acc, f1, size, evo_size\n")
+        for bench in [
+            "carcinogenesis",
+            "hepatitis",
+            "lymphography",
+            "mammographic",
+            "mutagenesis",
+            "nctrer",
+            "premierleague",
+            "pyrimidine",
+        ]:
+            owlfile = f"../sml-benchmarks/{bench}/{bench}.owl"
+            pospath = f"../sml-benchmarks/{bench}/full/pos.txt"
+            negpath = f"../sml-benchmarks/{bench}/full/neg.txt"
+
+            print("== Loading {}".format(owlfile))
+            A = structure_from_owl(owlfile)
+
+            P: list[int] = []
+            with open(pospath, encoding="UTF-8") as file:
+                for line in file.readlines():
+                    ind = line.rstrip()
+                    if ind not in A.indmap:
+                        print(
+                            "[ERR] The positive example {} does not seem to occur in {}".format(
+                                ind, owlfile
+                            )
+                        )
+                        sys.exit(1)
+                    P.append(A.indmap[ind])
+
+            N: list[int] = []
+            with open(negpath, encoding="UTF-8") as file:
+                for line in file.readlines():
+                    ind = line.rstrip()
+                    if ind not in A.indmap:
+                        print(
+                            "[ERR] The negative example {} does not seem to occur in {}".format(
+                                ind, owlfile
+                            )
+                        )
+                        sys.exit(1)
+                    N.append(A.indmap[ind])
+
+            sigma = determine_relevant_symbols(A, P + N, 1, 10)
+            inst = Instance(
+                A,
+                P,
+                N,
+                sigma,
+                frozenset([OP.ALL, OP.EX, OP.OR, OP.AND, OP.NEG, OP.LE, OP.GE]),
+                2,
+            )
+            (avg_acc, avg_f1, avg_size, avg_evo_size) = kfold(
+                inst, 10, max_k=10, timeout=300
+            )
+            _ = outfile.write(
+                f"{bench}, {avg_acc}, {avg_f1}, {avg_size}, {avg_evo_size} \n"
+            )
 
 
 def main():
