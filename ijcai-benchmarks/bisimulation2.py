aa96b2798bdeaec1d3e0649433d9915dba6b78cf
mfunk <mfunk@informatik.uni-leipzig.de>
2025-12-19 12:30:17 +0100

    Benchmark wip

--- ijcai-benchmarks/bisimulation2.py
+++ ijcai-benchmarks/bisimulation2.py
@@ -6,15 +6,18 @@ from spell.fitting_alc import FittingALC
 
 
 def main():
-    workers = [8]
-    runs = 3
+    workers = 8
+    runs = 1
     benchmarks = ["mammographic", "hepatitis", "lymphography"]
-    max_k = 8
-    language = "alc"
+    language = "alcq"
+    timeout = 10
+    max_k = 15
 
-    factors = {}
+    accuracies_with = {}
+    accuracies_without = {}
     for benchmark in benchmarks:
-        factors[benchmark] = []
+        accuracies_with[benchmark] = []
+        accuracies_without[benchmark] = []
 
         A = structure_from_owl(f"../sml-benchmarks/{benchmark}/{benchmark}.owl")
         pospath = f"../sml-benchmarks/{benchmark}/full/pos.txt"
@@ -42,12 +45,14 @@ def main():
                 P,
                 N,
                 op=frozenset(L_OP[language]),
-                workers=8,
+                workers=workers,
                 max_q=2,
-                bisim_reduction=False
+                bisim_reduction=False,
             )
 
-            acc, _, _ = f.solve_incr_approx(max_k)
+            acc, _, _ = f.solve_incr_approx(max_k, timeout = timeout)
+
+            accuracies_without[benchmark].append(acc)
 
             end = time.perf_counter()
 
@@ -61,22 +66,22 @@ def main():
                 P,
                 N,
                 op=frozenset(L_OP[language]),
-                workers=8,
+                workers=workers,
                 max_q=2,
                 bisim_reduction=True
             )
 
-            acc, _, _ = f.solve_incr_approx(max_k)
+            acc, _, _ = f.solve_incr_approx(max_k, timeout = timeout)
 
             end = time.perf_counter()
 
             print("==== TOOK {}".format(end - start))
             t2 = end - start
 
-            factors[benchmark].append(t2 / t1)
+            accuracies_with[benchmark].append(acc)
 
     for benchmark in benchmarks:
-        print(f"Benchmark {benchmark} : {sum(factors[benchmark]) / runs}s " )
+        print(f"Benchmark {benchmark} : {sum(accuracies_with[benchmark]) / runs} {sum(accuracies_without[benchmark]) / runs}" )
 
 
 if __name__ == "__main__":
