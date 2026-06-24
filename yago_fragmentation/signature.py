5fef49400d99b761ee0ef18cb77937daa649c80c
Draukjir <patrick.p1004@gmail.com>
2026-06-24 12:04:28 +0200

    created a new signature - removed some noisy classes

--- yago_fragmentation/signature.py
+++ yago_fragmentation/signature.py
@@ -12,45 +12,50 @@ class Signature:
         # concepts or roles which we are looking for in the 1st pass
         # determines the focus of which individuals will be in the domain
         self.domain_signature = {
-            "<http://yago-knowledge.org/resource/Actor>",
-            "<http://yago-knowledge.org/resource/Film_director>",
-            "<http://schema.org/Movie>",
-            "<http://yago-knowledge.org/resource/Author>",
+            "<http://yago-knowledge.org/resource/Actor>", # person who acts in a dramatic or comic production and works in film, television, theatre, or radio
+            "<http://yago-knowledge.org/resource/Composer>",
+            "<http://yago-knowledge.org/resource/Film_director>", 
+            "<http://yago-knowledge.org/resource/Painter>",
             "<http://yago-knowledge.org/resource/Scientist>",
-            "<http://yago-knowledge.org/resource/Musician>",
-            "<http://yago-knowledge.org/resource/Chef>",
-            "<http://yago-knowledge.org/resource/Album>"
+            "<http://yago-knowledge.org/resource/Singer>",
+            "<http://yago-knowledge.org/resource/Writer>" # person who uses written words to communicate ideas and to produce literary works
         }
         
         # concept_names which will be in the domain - 3rd pass
         # determines which concepts will stay in the domain
         self.concept_names = {
-            "<http://yago-knowledge.org/resource/Actor>",
-            "<http://yago-knowledge.org/resource/Film_director>",
-            "<http://schema.org/Movie>",
-            "<http://yago-knowledge.org/resource/Author>",
+            "<http://yago-knowledge.org/resource/Actor>", # person who acts in a dramatic or comic production and works in film, television, theatre, or radio
+            "<http://yago-knowledge.org/resource/Composer>",
+            "<http://yago-knowledge.org/resource/Film_director>", 
+            "<http://yago-knowledge.org/resource/Painter>",
             "<http://yago-knowledge.org/resource/Scientist>",
-            "<http://yago-knowledge.org/resource/Musician>",
-            "<http://yago-knowledge.org/resource/Chef>",
+            "<http://yago-knowledge.org/resource/Singer>",
+            "<http://yago-knowledge.org/resource/Writer>", # person who uses written words to communicate ideas and to produce literary works
+
+            # Supporting Concepts:
             "<http://yago-knowledge.org/resource/Album>",
-            "<http://yago-knowledge.org/resource/University>", #needed for the alumniOf Relation
-            "<http://yago-knowledge.org/resource/Prize>", #needed for the award Relation
-            "<http://schema.org/City>" #needed for the birthPlace Relation
+            "<http://yago-knowledge.org/resource/Art>", #general concept that creates expressive work for its beauty or emotional power (use Q838948 for the resulting work, use Q2018526 for the group of creative disciplines)
+            "<http://schema.org/Movie>",
+            "<http://schema.org/MusicComposition>",
+            "<http://yago-knowledge.org/resource/Prize>",
+            "<http://yago-knowledge.org/resource/Restaurant>",
+            "<http://yago-knowledge.org/resource/University>",
+            "<http://yago-knowledge.org/resource/Work_of_art>" #aesthetic item or artistic creation
         }
 
         # role_names - 2nd pass
         self.role_names = {
-            "<http://schema.org/spouse>",
             "<http://schema.org/actor>",
-            "<http://schema.org/director>",
-            "<http://schema.org/award>",
-            "<http://schema.org/author>",
-            "<http://schema.org/musicBy>",
-            "<http://schema.org/birthPlace>",
             "<http://schema.org/alumniOf>",
-            "<http://schema.org/productionCompany>",
-            "<http://yago-knowledge.org/resource/partOf>"
-
+            "<http://schema.org/author>", #The author of a CreativeWork or FictionalEntity
+            "<http://schema.org/award>",
+            "<http://schema.org/director>",
+            "<http://yago-knowledge.org/resource/fieldOfWork>",
+            "<http://schema.org/memberOf>",
+            "<http://schema.org/musicBy>",
+            "<http://yago-knowledge.org/resource/notableWork>",
+            "<http://schema.org/performer>",
+            "<http://yago-knowledge.org/resource/ownedBy>"
         }
 
         # target concept for definition extraction
